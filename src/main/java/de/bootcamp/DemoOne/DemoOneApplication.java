a94c9fc8c8958f349941ba9988bb25e39f60da76
anena <anenada@gmail.com>
2018-03-02 19:12:29 +0100

    Iitial commit

---
+++ src/main/java/de/bootcamp/DemoOne/DemoOneApplication.java
@@ -0,0 +1,12 @@
+package de.bootcamp.DemoOne;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class  DemoOneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoOneApplication.class, args);
+	}
+}
