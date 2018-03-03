1c08345f503e94ea96e914781378fa4edfe17222
anena <anenada@gmail.com>
2018-03-03 11:22:13 +0100

    Added scripts, to build War/ Jar.

--- src/main/java/de/webapp/spring/one/Application.java
+++ src/main/java/de/webapp/spring/one/Application.java
@@ -4,10 +4,11 @@ package de.webapp.spring.one;
 import de.webapp.spring.one.users.UserTestDataLoader;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
-public class Application {
+public class Application extends SpringBootServletInitializer {
 
 	@Bean(initMethod = "loadData")
 	UserTestDataLoader RepositoryTestData() {
